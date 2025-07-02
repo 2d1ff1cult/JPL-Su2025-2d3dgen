ea13a8bdf88bfbc043871e34bc76fc7e945e03fb
Jerome Ariola <48054365+2d1ff1cult@users.noreply.github.com>
2025-07-01 22:05:32 -0700

    updated bootstrap installer script

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,8 +1,5 @@
 @echo off
 
-:: reserved for when using local machine, copy into a .BAT script
-:: uses venv to make a virtual environment and isolate py installation
-:: see next cell for more info on a colab-friendly version
 :: MAKE SURE TO DOWNLOAD PYTHON 3.10 AND INSTALL AS FOLLOWS:
 :: do NOT install for all users
 :: add to path
@@ -17,20 +14,25 @@ py -3.10 -m venv 2d3dgen
 :: activate virtual environment
 call .\2d3dgen\Scripts\activate
 
+pip install qwen-vl-utils==0.0.11
+pause
+
 :: for debugging; comment later
 @echo on
 call
 @echo off
-echo Pausing to validate env start; should see (2d3dgen) prefixed to shell
-pause
+:: echo Pausing to validate env start; should see (2d3dgen) prefixed to shell
 
 :: setting up cadquery
 pip install cadquery==2.5.2
 pip install CQ-editor
-pause
+
+:: replicate the pip installs from the Dockerfile from cadrille github:
+pip install accelerate==0.34.2 cadquery-ocp==7.7.2 casadi==3.6.7 einops==0.8.0 transformers==4.50.3 flash-attn==2.7.2.post1 manifold3d==3.0.0 trimesh==4.5.3 contourpy==1.3.1 scipy==1.14.1 imageio==2.36.1 scikit-image==0.25.0 ipykernel==6.29.5 ipywidgets==8.1.5 pytorch3d==0.7.4 cadquery==2.5.2
+pip install open3d==0.19.0
 
 :: clone repos and install reqs
-git clone https://github.com/NVlabs/PartPacker.git
+git clone https://gitub.com/NVlabs/PartPacker.git
 cd PartPacker
 pip install -r requirements.txt
 pip install -r requirements.lock.txt
@@ -57,10 +59,8 @@ pause
 echo Changing back to directory of this script
 cd ../../
 dir
-:: replicate the pip installs from the Dockerfile from cadrille github:
-pip install -r cadrille_requirements.txt
-git clone https://github.com/col14m/cadrille.git
 
+git clone https://github.com/col14m/cadrille.git
 copy chat_cadrille.py cadrille\
 
 :: clear install cache and remove all wheels
